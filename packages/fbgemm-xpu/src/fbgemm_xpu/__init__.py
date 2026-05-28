4f59cec26f7537100ea5640933d0db9d583338ff
Felipe Leza Alvarez <felipe.leza.alvarez@intel.com>
2026-05-28 18:42:25 +0000

    Update cmake file to search in the fbgemm paths

--- packages/fbgemm-xpu/src/fbgemm_xpu/__init__.py
+++ packages/fbgemm-xpu/src/fbgemm_xpu/__init__.py
@@ -1,3 +1,20 @@
-# Main package initialization for the fbgemm_xpu module.
-# Imports the compiled C extension (_C) once sources are added under this dir.
-from . import _C  # noqa: F401
+# Copyright 2026 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Portions of this file are derived from FBGEMM
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Main package initialization for the fbgemm module
+# This imports the C extension and Python operator wrappers
+import torch
+from pathlib import Path
+
+# Import the compiled C extension (_C) which contains the registered operators
+# Import ops module which provides Python wrapper functions with autograd support
+from . import _C, ops
