2e75207d976ac714eb8eef20ce18f3153b55637e
Felipe Leza Alvarez <felipe.leza.alvarez@intel.com>
2026-06-06 00:28:02 +0000

    Add an operator template to build the package

---
+++ packages/fbgemm-xpu/src/fbgemm_xpu/ops.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2026 Intel Corporation. All Rights Reserved.
+#
+# Portions of this file are derived from FBGEMM
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Python wrapper functions for all custom operators under the fbgemm namespace
+# This module provides user-friendly interfaces to the C++ operators
+
+import torch
+from torch import Tensor
+
+__all__ = [
+	"dense_embedding_codegen_lookup_function",
+]
+
+def dense_embedding_codegen_lookup_function(*args, **kwargs):
+	"""Temporary stub for the planned dense embedding API."""
+	raise NotImplementedError(
+		"dense_embedding_codegen_lookup_function is not implemented yet in src/fbgemm_xpu/ops.py"
+	)
